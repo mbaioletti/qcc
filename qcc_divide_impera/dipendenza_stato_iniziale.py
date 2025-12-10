d6a4d7dc8fae68cc0541cb50bb599aad8e5203fd
mbaioletti <marco.baioletti@yahoo.it>
2025-12-10 21:56:48 +0100

    modificato script python dipendenza_stato_iniziale

--- qcc_divide_impera/dipendenza_stato_iniziale.py
+++ qcc_divide_impera/dipendenza_stato_iniziale.py
@@ -3,16 +3,15 @@ import os
 import subprocess
 df=pd.read_csv("../revlib/tutti.csv", sep=",")
 
-files=["rd53_131","rd53_251"]
+files=["miller_11.qasm", "4gt13_91.qasm", "0410184_169.qasm", "ex3_229.qasm", "sf_276.qasm", "rd53_131.qasm","rd53_251.qasm", "sqrt8_260.qasm", "misex1_241.qasm"]
 
 for tm in [10]:
     for f in files:
-        riga=df[df.instance_name==(f+".qasm")]
+        riga=df[df.instance_name==f].iloc[0,:]
         sz=int(riga['size'])
         de=int(riga.depth)
         qb=int(riga['qubits'])
         if sz<100000:
-            procs=[]
             if sz<500:
                 nch=1
             elif sz<1000:
@@ -26,11 +25,13 @@ for tm in [10]:
             else:
                 nch=150
             print(f"{f}, {sz}, {qb}")
-"""            for s in range(1,11):
-                cmd=f"./compile_qasm ../revlib/architectures/ibmq_tokyo.arch ../revlib/examples/{riga.instance_name}.qasm -timeout {tm} -bandit 1 -divide {nch} -res esp_swap_one.csv -seed {s}"
-                elenco=cmd.split(' ')
-                proc=subprocess.Popen(elenco)
-                procs.append(proc)
-            for p in procs:
-                p.wait()
-"""
+            for si in range(0,50+1):
+                procs=[]
+                for sa in range(1,15+1):
+                    cmd=f"./compile_qasm ../revlib/architectures/ibmq_tokyo.arch ../revlib/examples/{f} -timeout {tm} -bandit 1 -divide {nch} -res stato_iniz.csv -seed {sa} -seed_is {si}"
+                    elenco=cmd.split(' ')
+                    proc=subprocess.Popen(elenco)
+                    procs.append(proc)
+                for p in procs:
+                    p.wait()
+
