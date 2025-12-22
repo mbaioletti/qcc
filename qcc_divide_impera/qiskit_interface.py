f8cad6c349352cb30fbdfc82b73d3a9af3868f15
mbaioletti <marco.baioletti@yahoo.it>
2025-12-22 19:15:23 +0100

    funziona anche il passaggio da C++ a Python, non va bene la conversione da lista di gate a circuito Qiskit

--- qcc_divide_impera/qiskit_interface.py
+++ qcc_divide_impera/qiskit_interface.py
@@ -1,5 +1,6 @@
 from ctypes import cdll, c_int, c_wchar_p, Structure, POINTER, byref, pointer
 from qiskit import QuantumCircuit
+from qiskit.circuit import Gate
 lib=cdll.LoadLibrary("./libcirc.so")
 import numpy as np
 
@@ -12,7 +13,7 @@ class DirshGate(Structure):
                 
 lib.dirsh.restype = POINTER(DirshGate)
 
-def run_dirsh(qc, fname_arch):
+def convert_to_dirsh(qc):
     n=len(qc.data)
     gates_creator=DirshGate*n
     c=gates_creator()
@@ -21,8 +22,36 @@ def run_dirsh(qc, fname_arch):
         c[i].num_qubits=qc.data[i].operation.num_qubits
         c[i].qb1=qc.data[i].qubits[0]._index
         c[i].qb2=qc.data[i].qubits[1]._index if qc.data[i].operation.num_qubits==2 else -1
-    c1=lib.dirsh(c, n, fname_arch)
-    return c1
+    return c
+
+def find_num_qubits(c1):
+    n=0
+    i=0
+    while c1[i].name != "end":
+        n=max(n,c1[i].qb1)
+        if c1[i].num_qubits==2:
+            n=max(n,c1[i].qb2)
+        i +=1
+    return n+1
+    
+def convert_to_qiskit(c1):
+    nqb=find_num_qubits(c1)
+    qc2=QuantumCircuit(nqb)
+    i=0
+    while c1[i].name != "end":
+        g=Gate(c1[i].name, c1[i].num_qubits, [])
+        l=[c1[i].qb1]
+        if c1[i].num_qubits==2:
+            l.append(c1[i].qb2)
+        qc2.append(g, l)
+        i += 1
+    return qc2
+    
+def run_dirsh(qc, fname_arch):
+    c=convert_to_dirsh(qc)
+    c1=lib.dirsh(c, len(qc.data), fname_arch)
+    qc2=convert_to_qiskit(c1)
+    return qc2
     
 
 qc=QuantumCircuit(2,2)
